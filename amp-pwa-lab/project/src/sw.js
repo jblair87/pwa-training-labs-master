70b6a90d08d8dea9f4a7d35c0bccae710b0f55c1
Jenna Mandel <jenna.b.mandel@gmail.com>
2019-11-30 17:43:31 -0500

    add code to service worker js file

--- amp-pwa-lab/project/src/sw.js
+++ amp-pwa-lab/project/src/sw.js
@@ -13,3 +13,38 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'index.html',
+  'styles/main.css',
+  'images/space1.jpg',
+  'images/space2.jpg',
+  'images/space3.jpg'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
